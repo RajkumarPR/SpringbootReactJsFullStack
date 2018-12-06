777b50aa44c7e1205915604ad0d9ddb2f53bbedd
rraigonde <raju.raigonde@gmail.com>
2018-12-07 03:09:31 +0530

    formating error usinf binding result

--- PPMToolFullStack/src/main/java/com/hid/ppmtool/controller/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/hid/ppmtool/controller/ProjectController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author rraigonde
@@ -28,7 +31,10 @@ public class ProjectController {
     @PostMapping("/project")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
         if (result.hasErrors()) {
-            return new ResponseEntity<>("Invalid project object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : result.getFieldErrors())
+            errorMap.put(error.getField(), error.getDefaultMessage());
+            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(projectService.saveOrUpdateProject(project), HttpStatus.CREATED);
     }
