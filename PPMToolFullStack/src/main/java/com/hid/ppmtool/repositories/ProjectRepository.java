87c529fac740e4eb581b8762c5c39e84192b763b
rraigonde <raju.raigonde@gmail.com>
2018-12-07 01:00:02 +0530

    database and repositories

---
+++ PPMToolFullStack/src/main/java/com/hid/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.hid.ppmtool.repositories;
+
+import com.hid.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author rraigonde
+ * @date 07-12-2018 00:55
+ */
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
